99355c1e5b0361654ac45e74c64647c78b5cbceb
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2022-09-22 11:03:00 +0900

    pxt.json, main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -2,35 +2,53 @@ input.onButtonPressed(Button.A, function () {
 	
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        # . . . #
-        # . . # .
-        # # # . .
-        # . . # .
-        # . . . #
-        `)
-    basic.showLeds(`
-        . # # # .
-        . . # . .
-        . . # . .
-        . . # . .
-        . # # # .
-        `)
-    basic.showLeds(`
-        # . . . #
-        # # . . #
-        # . # . #
-        # . . # #
-        # . . . #
-        `)
-    basic.showLeds(`
-        . # # # .
-        # . . . .
-        # . # # .
-        # . . . #
-        . # # # .
-        `)
+	
 })
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            # . . . #
+            # . . # .
+            # # # . .
+            # . . # .
+            # . . . #
+            `)
+        basic.showLeds(`
+            . # # # .
+            . . # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+        basic.showLeds(`
+            # . . . #
+            # # . . #
+            # . # . #
+            # . . # #
+            # . . . #
+            `)
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # . # # .
+            # . . . #
+            . # # # .
+            `)
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showLeds(`
+            . # # # .
+            # . . . .
+            # . # # .
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
 })
