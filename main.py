a2bc6ab147cdff26707169ec7f5ff81b027e4c62
NANONANDFLASH <68502584+NANONANDFLASH@users.noreply.github.com>
2022-09-22 11:19:48 +0900

    main.blocks, main.ts, main.py 업데이트하기

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-input.is_gesture(Gesture.SHAKE)
+밝기 = 0
 
 def on_forever():
-    
-    basic.show_number(input.temperature())
+    global 밝기
+    밝기 = input.light_level()
+    if 밝기 >= 20:
+        basic.show_string("day")
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-input.isGesture(Gesture.Shake)
+let 밝기 = 0
 basic.forever(function on_forever() {
-    basic.showNumber(input.temperature())
+    
+    밝기 = input.lightLevel()
+    if (밝기 >= 20) {
+        basic.showString("day")
+    }
+    
 })
